a9d3167b168c4d06a2e5c034174e6fcef229f782
crypticsy <animesh19basnet@gmail.com>
2023-10-15 17:17:12 +0100

    🎁 implemented website publication from local

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        
+      ]
+    })
+  ],
+  base : '',
+})
